552a151b0313c2fc6ee1be3bbf70fd5a4b06fdc0
David Rorex <davidr@vyew.com>
2012-01-20 20:58:18 -0800

    Initial dumb AI (find lowest spot piece will fit)

--- tetris.ino
+++ tetris.ino
@@ -1,3 +1,4 @@
+#include <avr/pgmspace.h>
 
 byte pieces[] = { 
 0xF0,	// 1111
@@ -19,6 +20,38 @@ byte pieces[] = {
 	// 1100
 };
 
+// Lookup table for piece rotation, to speed things up (in theory...should probably benchmark it)
+PROGMEM prog_uchar rotation_map[] = {
+0xf0, 0x00, 0x44, 0x44,
+0x44, 0x44, 0x00, 0x0f,
+0x00, 0x0f, 0x22, 0x22,
+0x22, 0x22, 0xf0, 0x00,
+0x4e, 0x00, 0x80, 0x8c,
+0x80, 0x8c, 0x00, 0x72,
+0x00, 0x72, 0x31, 0x01,
+0x31, 0x01, 0x4e, 0x00,
+0xe8, 0x00, 0x40, 0xc4,
+0x40, 0xc4, 0x00, 0x17,
+0x00, 0x17, 0x23, 0x02,
+0x23, 0x02, 0xe8, 0x00,
+0x8e, 0x00, 0x80, 0xc8,
+0x80, 0xc8, 0x00, 0x71,
+0x00, 0x71, 0x13, 0x01,
+0x13, 0x01, 0x8e, 0x00,
+0x6c, 0x00, 0x40, 0x8c,
+0x40, 0x8c, 0x00, 0x36,
+0x00, 0x36, 0x31, 0x02,
+0x31, 0x02, 0x6c, 0x00,
+0xc6, 0x00, 0x80, 0x4c,
+0x80, 0x4c, 0x00, 0x63,
+0x00, 0x63, 0x32, 0x01,
+0x32, 0x01, 0xc6, 0x00,
+0xcc, 0x00, 0x00, 0xcc,
+0x00, 0xcc, 0x00, 0x33,
+0x00, 0x33, 0x33, 0x00,
+0x33, 0x00, 0xcc, 0x00,
+};
+
 
 byte piece[10][2];
 
@@ -27,14 +60,88 @@ signed char py[10];
 
 byte board[10][32]; // 16x16 / 8
 
-void run_board(byte b) {
-  int board_x = b*11;
-  int board_y = 0;
+byte targetpos[10];
+byte targetrot[10];
 
-  if(piece[b][0] == 0 && piece[b][1] ==  0x00){
-    piece[b][0] = pieces[rand()%6];
+// rotate a piece using our lookup table
+void rotate_piece(byte b) {
+  byte *p=piece[b];
+  byte p1=p[0];
+  byte p2=p[1];
+  for(byte i=0; i<28*4; i+=4) {
+    if(pgm_read_byte_near(rotation_map+i  ) == p1
+    && pgm_read_byte_near(rotation_map+i+1) == p2
+      ) {
+	p[0] = pgm_read_byte_near(rotation_map+i+2);
+	p[1] = pgm_read_byte_near(rotation_map+i+3);
+      }
+  }
+}
+
+void find_best_spot(byte b) {
+  byte best_x=px[b];
+  byte best_rot=0;
+  byte best_score=0;
+
+  // loop thru possible rotations
+  for(byte rot=0; rot<3; rot++) {
+
+    // loop thru possible positions
+    for(byte px=0; px<10; px++) {
+
+      byte py=0;
+      byte hity=0;
+      while(py < 15) { // drop the piece down until it hits
+	for(byte y=0; y<4 && py<100; y++) {
+	  for(byte x=0; x<4 && py<100; x++) {
+	    if( (piece[b][y>1] & 1<<(x+(y&1)*4))
+	      && (
+		x+px>9 || y+py>14
+		(board[b][(y+py)*2 + ((x+px)/8)] & 1<<((x+px)&7))
+		)
+	      ) {
+	      hity=py;
+	      py=100;
+	      break;
+	    }
+	  }
+	}
+	if(hity==0) py++;
+      }
+      byte score = hity;
+      if(score>best_score || (score == best_score && rand()&7==1)) {
+	best_score = score;
+	best_rot = rot;
+	best_x = x;
+      }
+     // if(py[b] > 15) hit=1;
+    }
+    rotate_piece(b);
   }
 
+  targetpos[b] = best_x;
+  targetrot[b] = best_rot;
+}
+
+void run_board(byte b) {
+  int board_x = b*11+1;
+  int board_y = 0;
+
+  // if no piece
+  if(piece[b][0] == 0 && piece[b][1] == 0x00){
+    // pick a random piece
+    piece[b][0] = pieces[rand()%6];
+
+    // rotate it randomly
+    for(byte r=rand()&3; r<3; r++) {
+      rotate_piece(b);
+    }
+
+    // call tetris AI to find a target location for it
+    find_best_spot(b);
+  }
+
+  // add in some slowness so game runs at same speed regardless of number of tetrises
   if(py[b] < 0) {
     delay(6);
     py[b]++;
@@ -44,7 +151,18 @@ void run_board(byte b) {
   for(byte y=0;y<16;y++) {
     setpx(board_x+10, y, 1);
   }
+
+  // move piece if needed
+  if(px[b] < targetpos[b])
+    px[b]++;
+  else if(px[b] > targetpos[b])
+    px[b]--;
   
+  // rotate piece if needed
+  if(targetrot[b] > 0) {
+    rotate_piece[b];
+    targetrot[b]--;
+  }
   
   // draw the board on the scren
   for(byte y=0;y<16;y++)
